746fb021d010cc483753e021a773abad0f6f62e5
hoangdl <hoangledinh65@gmail.com>
2022-07-12 16:26:20 +0700

    update

--- first-demo/loadbalancer.tf
+++ first-demo/loadbalancer.tf
@@ -1,7 +1,7 @@
-resource "aws_lb" "hoangdl-alb" {
-  name               = "hoangdl-alb"
-  internal           = true
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.hoangdl-sg.id]
-  subnets            = [for subnet in data.aws_subnet.hoangdl-subnet : subnet.id]
-}
+# resource "aws_lb" "hoangdl-alb" {
+#   name               = "hoangdl-alb"
+#   internal           = true
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.hoangdl-sg.id]
+#   subnets            = [for subnet in data.aws_subnet.hoangdl-subnet : subnet.id]
+# }
